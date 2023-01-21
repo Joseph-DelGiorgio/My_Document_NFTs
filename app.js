518f14e6ed47e8a384ded526301df274fc177cf0
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-21 14:26:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -136,3 +136,68 @@ function App() {
 }
 export default App;
 
+
+
+
+
+
+
+
+
+
+
+
+//Version 2
+
+
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+how 
+
+function App() {
+  const API_KEY = 'https://arb-goerli.g.alchemy.com/v2/wzo69fqo0ZyJ2SV0d14SnLdJh39J7iXj';
+  const web3 = new Web3(new Web3.providers.HttpProvider(API_KEY));
+  const networkId = await web3.eth.net.getId();
+  const contractAddress = abi.networks[networkId].address;
+  const contract = new web3.eth.Contract(abi.abi, contractAddress);  
+
+  useEffect(() => {
+    if (window.ethereum) {
+      setWeb3(new Web3(window.ethereum));
+    } else if (window.web3) {
+      setWeb3(new Web3(window.web3.currentProvider));
+    } else {
+      // Connect to the provider
+      const web3 = new Web3(API_KEY);
+      setWeb3(web3);
+    }
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const contract = await generateABI("./contracts/SoulBound_Token.sol");
+        console.log(contract);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+
+  const handleMintSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const degree = form.elements["degree"].value;
+    const school = form.elements["school"].value;
+    const year = form.elements["year"].value;
+    await contract.methods.mint(degree, school, year).send({ from: account });
+  }
+
+}
+
+export default App;
+
+
+
