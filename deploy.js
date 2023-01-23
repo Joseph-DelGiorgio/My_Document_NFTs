4698fc8d59d343c9119fb35e7cdeff3158ae84fb
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-23 12:27:19 -0800

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,14 +1,16 @@
 const { deploy } = require("@nomicfoundation/hardhat-chai-matchers");
-require("@nomiclabs/hardhat-ethers");
+const SchoolDegrees = require(`/Users/josephdelgiorgio/SoulBound_Token_Degree/artifacts/contracts/SchoolDegrees.sol/SchoolDegrees.json`);
+const { provider } = require("@nomiclabs/hardhat-ethers");
+
 require('dotenv').config()
 
-const SchoolDegrees = require(`/Users/josephdelgiorgio/soul_bound_token/contracts/SchoolDegrees.sol`);
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const signers = await provider.getSigners();
+
 
   console.log("Deploying contract...");
-  const contract = await deploy(SchoolDegrees, [], { deployer });
+  const contract = await deploy(SchoolDegrees, [], { provider });
 
   console.log("Contract deployed at:", contract.address);
 }
