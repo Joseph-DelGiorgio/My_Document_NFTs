20e2b2e51e51a65c5b1423c057504274cb729f71
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-23 16:27:17 -0800

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,23 +1,22 @@
-const { deploy } = require("@nomicfoundation/hardhat-chai-matchers");
-const SchoolDegrees = require(`/Users/josephdelgiorgio/SoulBound_Token_Degree/artifacts/contracts/SchoolDegrees.sol/SchoolDegrees.json`);
-const { provider } = require("@nomiclabs/hardhat-ethers");
-
-require('dotenv').config()
-
+const { ContractFactory } = require("ethers");
+const { ethers } = require("hardhat");
+const SchoolDegreesArtifact = require("/Users/josephdelgiorgio/SoulBound_Token_Degree/artifacts/contracts/SchoolDegrees.sol/SchoolDegrees.json");
 
 async function main() {
-  const signers = await provider.getSigners();
-
+  const accounts = await ethers.getSigners();
+  const from = accounts[0];
 
   console.log("Deploying contract...");
-  const contract = await deploy(SchoolDegrees, [], { provider });
+  const contractFactory = new ContractFactory(SchoolDegreesArtifact.abi, SchoolDegreesArtifact.bytecode, from);
+  const contract = await contractFactory.deploy();
 
   console.log("Contract deployed at:", contract.address);
 }
 
+
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+.then(() => process.exit(0))
+.catch((error) => {
+console.error(error);
+process.exit(1);
+});
