4bf2037e497ba537c03ff6e3329a131894ccdc08
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-23 17:07:22 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,13 @@ require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.0",
+  solidity:{
+  version: "0.8.17",
   networks: {
     goerli: {
       url: process.env.API_KEY,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
+}
 };
-
