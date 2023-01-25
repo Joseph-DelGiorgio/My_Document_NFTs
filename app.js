8e17e8de8c2370ce4107fa7e8346d0751b5232df
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-25 12:10:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,84 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 import Web3 from "web3";
-import fetchAbi from './fetchabi.js'
-//import PageButton from './../components/PageButton'
-//import ConnectButton from "./ConnectButton";
-//import fetchAbi from '/Users/josephdelgiorgio/SoulBound_Token_Degree/my-app/src/fetchabi.js'
-//const address= (0x5FbDB2315678afecb367f032d93F642f64180aa3);
+//import ethers from '/Users/josephdelgiorgio/SoulBound_Token_Degree/my-app/package.json';
+//import abi from "/Users/josephdelgiorgio/SoulBound_Token_Degree/my-app/src/build/contracts_SoulBound_Token_sol_SchoolDegrees.abi";
+//console.log(abi);
+//const contract = Web3.eth.Contract(abi, contractAddress);
+//contract= contractAddress;
 
 
 function App(){
-  const [web3, setWeb3] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [account, setAccount] = useState(null);
-  const fetchAbi = require('./fetchabi.js').fetchAbi;
+const [web3, setWeb3] = useState(null);
+const [contract, setContract] = useState(null);
+const [account, setAccount] = useState(null);
 
-  //const API_KEY = 'https://arb-goerli.g.alchemy.com/v2/wzo69fqo0ZyJ2SV0d14SnLdJh39J7iXj';
-
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.enable().then(() => {
-        setWeb3(new Web3(window.ethereum));
-      });
-    } else if (window.web3) {
-      setWeb3(new Web3(window.web3.currentProvider));
+useEffect(() => {
+    if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+        setWeb3(new Web3(Web3.givenProvider)); // connect to the injected web3 provider
+        getCurrentAccount();
     }
-  }, []);
+}, []);
 
-  useEffect(() => {
+useEffect(() => {
     async function fetchData() {
-      try {
-        const abi = await fetchAbi();
-        const networkId = await web3.eth.net.getId();
-        const contractAddress = abi.networks[networkId].address;
-        const contract = new web3.eth.Contract(abi, contractAddress);  
-        setContract(contract);
-        const accounts = await web3.eth.getAccounts();
-        setAccount(accounts[0]);
-      } catch (error) {
-        console.error(error);
-      }
+        try {
+            const contract= import("/Users/josephdelgiorgio/SoulBound_Token_Degree/my-app/src/artifacts/contracts/SoulBound_Token.sol/SchoolDegrees.json")
+            const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+            const abi= import("/Users/josephdelgiorgio/SoulBound_Token_Degree/my-app/src/build-info/src_SoulBound_Token_sol_SchoolDegrees.abi");
+            contract = new web3.eth.Contract(abi, contractAddress); // instantiate the contract with the ABI and address
+            setContract(contract);
+            const accounts = await web3.eth.getAccounts();
+            setAccount(accounts[0]);
+        } catch (error) {
+            console.error(error);
+        }
     }
-    fetchData();
-  }, []);
+    if(web3) {
+        fetchData();
+    }
+}, [web3]);
 
-  const ConnectButton = props => {
-    const { isConnected, signerAddress, getSigner, provider } = props
-    const displayAddress = `${signerAddress?.substring(0,10)}...`
+const getCurrentAccount = async () => {
+    if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+        try {
+            const accounts = await window.ethereum.request({
+                method: "eth_accounts",
+            });
+            if (accounts.length > 0) {
+                setAccount(accounts[0]);
+                console.log(accounts[0]);
+            } else {
+                console.log("Connect to MetaMask using the Connect button");
+            }
+        } catch (err) {
+            console.error(err.message);
+        }
+    } else {
+        /* MetaMask is not installed */
+        console.log("Please install MetaMask");
+    }
+};
 
-  }
-  const handleMintSubmit = async (event) => {
+const handleMintSubmit = async (event) => {
     event.preventDefault();
     if(contract === null) {
-        console.log("contract is not loaded yet, minting cannot be done.");
+        console.log("contract is not loaded yet, mint failed");
         return;
     }
-    const form = event.target;
-    const degree = form.elements["degree"].value;
-    const school = form.elements["school"].value;
-    const year = form.elements["year"].value;
-    await contract.methods.mint(degree, school, year).send({ from: account });
-  }
+    // Add minting logic here
+    // Example: contract.methods.mint(to, amount).send({ from: account })
+};
 
+return (
+    <div className="App">
+        <form onSubmit={handleMintSubmit}>
+            <input type="submit" value="Mint" />
+        </form>
+        <p>Account: {account}</p>
+    </div>
+);
+}
 
   return (
     <form onSubmit={handleMintSubmit}>
