8a8242ede4d13f417a133ebea8521e3930cd28dd
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-24 17:07:54 -0800

    Update fetchabi.js

--- fetchabi.js
+++ fetchabi.js
@@ -1,6 +1,6 @@
 const ethers = require('ethers');
-const abi = require('./contracts/SoulBound_Token.sol');
-const contractAddress = '0x123456789abcdef';
+const abi = require('/Users/josephdelgiorgio/SoulBound_Token_Degree/my-app/src/build/contracts_SoulBound_Token_sol_SchoolDegrees.abi');
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 
 async function fetchAbi() {
