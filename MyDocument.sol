0752994f13b908f4cfc1debd001142fa7b4848d2
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-30 16:06:37 -0800

    Update MyDocument.sol

--- MyDocument.sol
+++ MyDocument.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 //import '/Users/josephdelgiorgio/SBTD/artifacts/contracts/MyDegree.sol/MyDegree.json'
-
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
@@ -9,36 +10,58 @@ import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
 contract MyDegree is Initializable, ERC721Upgradeable, OwnableUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
-    struct DegreeInfo {
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    string internal baseTokenUri;
+    address payable public withdrawAccount;
+    mapping(address=>uint256) public documentMints;
+
+    struct documentInfo {
         string school;
         string degree;
         string field;
         string cid;
     }
-    mapping(uint256 => DegreeInfo) public degreeInfo;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
+    
     constructor() {
         _disableInitializers();
+        totalSupply= 0;
+        maxSupply= 10000;
+        mintPrice= 0.01 ether; //10000000000000000 wei
+    }
+
+    function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner{
+        baseTokenUri= baseTokenUri_;
+    }
+
+    function tokenUri(uint256 tokenId_) public view returns (string memory){
+        require(_exists(tokenId_), "Token is not valid");
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), "/Users/josephdelgiorgio/SBTD/my-app/src/MyDegree.json"));
     }
 
     function initialize() initializer public {
-        __ERC721_init("MyDegree", "MDG");
+        __ERC721_init("MyDocument", "MDT");
         __Ownable_init();
     }
 
-    function safeMint(address to) payable public{
-        require(msg.value == 0.5 ether, "Incorrect payment amount");
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+    function safeMint(address to, uint256 quantity_) payable public{
+        require(msg.value == mintPrice, "Incorrect payment amount, 0.01 ETH required");
+        require(totalSupply <= maxSupply, "No more Document Minting available");
+        for(uint256 i =0; i < quantity_; i++){
+            uint256 newTokenId = totalSupply +1;
+            _tokenIdCounter.increment();
+            _safeMint(to, newTokenId);
+        }
     }
 
 
-    function withdrawNFTRevenue(uint _amount) public{
-        payable (msg.sender).transfer(_amount);
+    function withdraw() external onlyOwner{
+        address withdrawAccount = 0x203EaDa017F04475193596A329017E926E3DAfe9;
+        withdrawAccount.call{ value: address(this).balance}('');
     }
+
 }
 
